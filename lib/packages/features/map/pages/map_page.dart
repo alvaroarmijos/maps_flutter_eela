cd6ce02c8879c40f430d34b0b71d06029f629bdb
Alvaro Armijos <56322173+alvaroarmijos@users.noreply.github.com>
2024-09-10 21:03:39 -0500

    Add google maps.

--- lib/packages/features/map/pages/map_page.dart
+++ lib/packages/features/map/pages/map_page.dart
@@ -1,13 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class MapPage extends StatelessWidget {
   const MapPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    const cameraPosition = CameraPosition(
+      target: LatLng(-4.000127, -79.203592),
+      zoom: 17,
+    );
     return const Scaffold(
-      body: Center(
-        child: Text('Map page ....'),
+      body: GoogleMap(
+        initialCameraPosition: cameraPosition,
       ),
     );
   }
